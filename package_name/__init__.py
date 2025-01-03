4367f1a93f7219b3540fbd5b7d386ec542cfa23b
zhangjinyiyi <zhangjinyi.cn@hotmail.com>
2025-01-03 13:08:30 +0800

    init

---
+++ package_name/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2025/01/03 13:07:49
+@Author  :   Jinyi Zhang 
+@Version :   1.0
+@Contact :   zhangjinyi.cn@hotmail.com
+@License :   (C)Copyright 2023-2024, Jinyi Zhang
+@Desc    :   None
+"""
